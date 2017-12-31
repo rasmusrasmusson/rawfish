76eda29454d18cb684b68ccdff301c03fc221951
kevin gates <kevinobamatheus@gmail.com>
2017-12-31 18:22:30 +0800

    Member signup

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -18,14 +18,18 @@
                 <label for="exampleFormControlInput1">Nickname</label>
                 <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
             </div>
-            <div class="row form-group">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="First name">
+
+            <div class="form-group">
+                <div class="col-md-6" style="padding-left: 0;">
+                    <label for="validationCustom01">First name</label>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
                 </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Last name">
+                <div class="col-md-6">
+                    <label for="validationCustom02">Last name</label>
+                    <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
                 </div>
             </div>
+
             <div class="form-group">
                 <label for="exampleFormControlInput1">Email / Mobile number</label>
                 <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
@@ -35,9 +39,27 @@
                 <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
             </div>
             <div class="form-group">
-                <p>By signing up with Raw Fish you agree to our Terms,Data use policy and
-                    Cookie use.
+                <p style="font-size: 10px;">By signing up with Raw Fish you agree to our Terms,Data use
+                    <a href="#" class="font-color-green">policy</a> and <a href="#" class="font-color-green">Cookie</a> use.
                 </p>
+            </div>
+
+            <div class="form-group text-center">
+
+                <input width="100" type="image" src="/images/signup-button.png" border="0" alt="Submit" />
+
+                {{--<svg width="100" height="100">--}}
+                    {{--<circle cx="50" cy="50" r="40" fill="#20FF00" />--}}
+                    {{--<text x="50%" y="50%" text-anchor="middle" fill="black" font-size="15px" dy=".3em">SIGN UP</text>--}}
+                    {{--Sorry, your browser does not support inline SVG.--}}
+                {{--</svg>--}}
+
+            </div>
+            <div class="form-group text-center">
+                <p class="">or</p>
+            </div>
+            <div class="form-group text-center">
+                <p><a href="#"><img width="200" src="/images/signup-with-wechat-link.png"></a></p>
             </div>
         </form>
 
