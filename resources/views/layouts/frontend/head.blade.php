f6e7e4b858356206eda441a7f628a96b0d5b5be6
kevin gates <kevinobamatheus@gmail.com>
2017-12-31 12:31:47 +0800

    mobile header

--- resources/views/layouts/frontend/head.blade.php
+++ resources/views/layouts/frontend/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="description" content="">
 <meta name="author" content="xxx">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>raw fish</title>
 
--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -1,58 +1,18 @@
-{{--<style>--}}
-    {{--.navbar {--}}
-        {{--min-height: 160px;--}}
-    {{--}--}}
-
-    {{--.navbar-brand {--}}
-        {{--padding: 0 15px;--}}
-        {{--height: 160px;--}}
-        {{--line-height: 160px;--}}
-    {{--}--}}
-
-    {{--.navbar-toggle {--}}
-        {{--/* (80px - button height 34px) / 2 = 23px */--}}
-        {{--margin-top: 23px;--}}
-        {{--padding: 9px 10px !important;--}}
-    {{--}--}}
-
-    {{--@media (min-width: 768px) {--}}
-        {{--.navbar-nav > li > a {--}}
-            {{--/* (80px - line-height of 27px) / 2 = 26.5px */--}}
-            {{--padding-top: 26.5px;--}}
-            {{--padding-bottom: 26.5px;--}}
-            {{--line-height: 27px;--}}
-        {{--}--}}
-    {{--}--}}
-{{--</style>--}}
-{{--<nav class="navbar navbar-default navbar-static-top">--}}
-        {{--<div class="navbar-header">--}}
-            {{--<a class="navbar-brand" href="/"><img height="30" src="/images/rawfish_logo.png"></a>--}}
-        {{--</div>--}}
-        {{--<div class="collapse navbar-collapse js-navbar-collapse">--}}
-            {{--<ul class="nav navbar-nav navbar-right">--}}
-                {{--<li><a href="#">LOGIN</a></li>--}}
-                {{--<li><a href="#">SIGN UP</a></li>--}}
-                {{--<li><a href="#">中/En</a></li>--}}
-                {{--<li><a href="#">More</a></li>--}}
-            {{--</ul>--}}
-        {{--</div>--}}
-{{--</nav>--}}
-
 <!-- Navigation -->
 <div class="row" style="background-color: white">
 &nbsp;
 </div>
 <nav class="navbar navbar-default navbar-static-top">
-        <div class="navbar-header">
+        <div class="navbar-header pull-left" >
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}" style="padding-top: 0;">
-                <img height="30" src="/images/rawfish_logo.png">
+                <img height="30"   src="/images/rawfish_logo.png">
             </a>
         </div>
 
         <div class="collapse navbar-collapse js-navbar-collapse" id="app-navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-            <li><a href="{{ url('/') }}" class="login">LOGIN</a></li>
+            <li ><a href="{{ url('/') }}" class="login">LOGIN</a></li>
             <li><a  >&nbsp;</a></li>            
             <li><a href="{{ url('/') }}" class="language">中 / <span class='english' style="color: #20FF00;">En</span></a></li>
             <li><a >&nbsp;</a></li>
@@ -60,4 +20,15 @@
             <li><a >&nbsp;</a></li>
             </ul>
         </div>
+
+        <!-- mobile -->
+        <div class="nav-mobile rawfish-nav">          
+          <ul>        
+            <li style="float: left;padding-top: 5px;">
+            <a href="{{ url('/') }}" class="language" >中 / <span class='english' style="color: #20FF00;">En</span></a></li>   
+         <li>
+            <a href="{{ url('/') }}" style=" "><img  height="30" src="/images/header/navigator.png"></a>  </li>
+          </ul>   
+         
+        <!-- mobile -->
 </nav>
